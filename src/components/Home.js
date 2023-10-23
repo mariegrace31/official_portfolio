7209b49faabf9c87171e3c1484ee9354c0d27f43
mariegrace31 <mariegracebmg@gmail.com>
2023-10-24 01:22:56 +0200

    Add the header

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../styles/Home.css';
+
+const Home = () => (
+  <div className="header" id="home">
+    <div class="presentation">
+        <h1>Hello &#128075;, Welcome to my world!</h1>
+        <p  class="about-me">
+          I'm <span>Marie-Grace Bahati</span>, <br/>
+          a passionate Full Stack Developer.
+        </p>
+        <a href="#projects">See my projects</a>
+      </div>
+  </div>
+);
+
+export default Home;
