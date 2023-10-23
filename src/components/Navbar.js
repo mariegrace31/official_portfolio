ce72eea6e66e090ad57f1279678e0058a636e544
mariegrace31 <mariegracebmg@gmail.com>
2023-10-24 00:32:39 +0200

    Update the navbar and style it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import '../styles/Navbar.css';
 
-const Navbar = () => (
-  <div className="nav-bar">
-    <p>
-      Bmg
-      <span>.</span>
-    </p>
-    <div className="Nav-links">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About me</a>
-        </li>
-        <li>
-          <a href="projects">Projects</a>
-        </li>
-        <li>
-          <a href="contact">Contact</a>
-        </li>
-      </ul>
+const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+  return (
+    <div className="nav-bar">
+      <p>
+        Bmg
+        <span>.</span>
+      </p>
+      <div className={`Nav-links ${mobileMenuOpen ? 'mobile-open' : ''}`}>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About me</a>
+          </li>
+          <li>
+            <a href="projects">Projects</a>
+          </li>
+          <li>
+            <a href="contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="mobile-menu-toggle" role="button" tabIndex="0" onKeyDown={toggleMobileMenu} onClick={toggleMobileMenu}>
+        {mobileMenuOpen ? (
+          <FaTimes className="close" />
+        ) : (
+          <FaBars className="bars" />
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
