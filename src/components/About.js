2a0c8e3d63f427526f0225e685980b0ee713e545
mariegrace31 <mariegracebmg@gmail.com>
2023-12-06 01:35:15 +0200

    Add the assets

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div className="about-section" id="about">
+    <h2>About</h2>
+    <hr className="underline"/>
+  </div>
+);
+
+export default About;
