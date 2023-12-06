f8a2fe525fd2a54dcb5eb4a40d541b829e4e91dd
mariegrace31 <mariegracebmg@gmail.com>
2023-12-06 21:31:28 +0200

    Style the mob version

--- src/components/About.js
+++ src/components/About.js
@@ -4,28 +4,31 @@ import { BiCodeBlock } from 'react-icons/bi';
 import { HiLightBulb } from 'react-icons/hi';
 import programmingGif from '../assets/ProgrammingGif.gif';
 import resume from '../assets/resume.pdf';
+import '../styles/About.css';
 
 const About = () => (
   <div className="about-section" id="about">
     <h2>About</h2>
     <hr className="underline" />
     <div className="aboutMe">
-      <p>
-        I&apos;m Marie-Grace, a tech-loving software engineer who is passionate about coding,
-        problem-solving, and collaborating with global peers to build innovative solutions.
-        I strongly believe in lifelong learning that&apos;s why I&apos;m always excited about
-        learning new things and improving myself.
-        Feel free to explore my portfolio to see my work and experience.
-        If you like what you see and have a project in mind, don&apos;t hesitate to
-        {' '}
-        <a href="#contact" className="reach">reach out.</a>
-        {' '}
-        <br />
-        {' '}
-        <span className="create">Let&apos;s create something amazing together!</span>
-      </p>
-      <a href={resume} target="_blank" className="resume" rel="noreferrer">Get my resume</a>
-      <img src={programmingGif} alt="Programming GIF" />
+      <div className="first-part">
+        <p>
+          I&apos;m Marie-Grace, a tech-loving software engineer who is passionate about coding,
+          problem-solving, and collaborating with global peers to build innovative solutions.
+          I strongly believe in lifelong learning that&apos;s why I&apos;m always excited about
+          learning new things and improving myself.
+          Feel free to explore my portfolio to see my work and experience.
+          If you like what you see and have a project in mind, don&apos;t hesitate to
+          {' '}
+          <a href="#contact" className="reach">reach out.</a>
+          {' '}
+          <br />
+          {' '}
+          <span className="create">Let&apos;s create something amazing together!</span>
+        </p>
+        <a href={resume} target="_blank" className="resume" rel="noreferrer">Get my resume</a>
+      </div>
+      <img className="gif" src={programmingGif} alt="Programming GIF" />
     </div>
     <div className="lang-tools">
       <div className="part">
@@ -59,7 +62,7 @@ const About = () => (
       </div>
       <div className="part">
         <h3>
-          <HiLightBulb />
+          <HiLightBulb className="skills" />
           Tech and soft skills
         </h3>
         <ul>
