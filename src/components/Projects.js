8898b365ffd281bcc3e3251aecf85d653d0f5543
mariegrace31 <mariegracebmg@gmail.com>
2023-12-08 17:31:41 +0200

    Add the projects part

---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Projects = () => (
+  <div className="projects-container" id="projects">
+    <h2>PROJECTS</h2>
+    <hr className="underline"/>
+  </div>
+)
+
+export default Projects;
