30adb9ee6069e843518c6d8fc81d2f3962942437
Marie Grâce Bahati <mariegracebmg@gmail.com>
2023-12-10 23:35:42 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import React from "react";
 const Projects = () => (
   <div className="projects-container" id="projects">
     <h2>PROJECTS</h2>
+    <div></div>
     <hr className="underline"/>
   </div>
 )
