05f5df6eb15239c2e897f6c64b83d93b3c5f717d
mariegrace31 <mariegracebmg@gmail.com>
2023-12-11 21:02:47 +0200

    Add projects images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,10 @@
 import React from "react";
+import bookstore from '../assets/bookstore.JPG';
+import mealGallery from '../assets/mealGallery screeshot.png';
+import calculator from '../assets/Capture2.JPG';
+import airPollution from '../assets/Capture1.JPG';
+import budgetBuddy from '../assets/BuggetBuddy.JPG';
+import toDo from '../assets/to-do.JPG';
 
 const Projects = () => (
   <div className="projects-container" id="projects">
